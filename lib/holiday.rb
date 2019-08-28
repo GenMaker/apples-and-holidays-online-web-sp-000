8eeba8d249bbc92ed9a596b6e4e75f9a5d447c52
Panela Leung <leungpanela@gmail.com>
2019-08-28 18:49:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_supplies_in_holidays(holiday_hash)
           puts"#{season.capitalize}:"
           holiday_values.collect do |name, supplies|
             binding.pry
-            name.to_s.split("_").map{|word| word.capitalize}.join (" ")
+            puts name.to_s.split("_").map{|word| word.capitalize}.join (" ")
             #supplies.to_s.capitalize
           end
         #return transformed string
