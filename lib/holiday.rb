02a4e316ab694e2a08d54982af5cf781cd62fb09
Panela Leung <leungpanela@gmail.com>
2019-08-28 19:51:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,6 +81,6 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, data|
     data.each do |holiday,supplies|
-      supplies.
+      supplies.include
 
 end
