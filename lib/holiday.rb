5394eb3d8717c9a013c8020e40d75aeb0244b685
Panela Leung <leungpanela@gmail.com>
2019-08-28 19:02:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_supplies_in_holidays(holiday_hash)
             #binding.pry
             puts name.to_s.split("_").map{|word| word.capitalize}.join (" ")
           supplies.collect do |item|
-            puts item.to_s.split("_").map{|word|word.capitalize.join (" ")   
+            puts item.to_s.split("_").map{|word|word.capitalize.join (" ")
           end
           end
         #return transformed string
@@ -97,11 +97,10 @@ def all_supplies_in_holidays(holiday_hash)
       #       "#{holiday_values}:"
       #   end
       # end
-  end
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
 end
